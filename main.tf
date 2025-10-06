d651b9a1e114a9918d8f45ea2eb64fdd183b2db8
Vinayravuri55 <vinayravuri55@gmail.com>
2025-10-06 12:07:14 +0530

    linux1

--- main.tf
+++ main.tf
@@ -2,32 +2,28 @@ data "azurerm_resource_group" "example" {
   name     = "813-cd34c1c0-hands-on-with-terraform-on-azure" #need to update
 }
 
-resource "azurerm_app_service_plan" "example" {
+resource "azurerm_service_plan" "example" {
   name                = "${var.company}${var.environment}asp"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  os_type             = "Linux"
+  sku_name = "S1"
 }
 
-resource "azurerm_app_service" "example" {
+resource "azurerm_linux_web_app" "example" {
   name                = "${var.company}${var.environment}as"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
-    site_config {
-      dotnet_framework_version = "v6.0"
-      scm_type                 = "LocalGit"
-    }
+  service_plan_id = azurerm_service_plan.example.id
+  site_config {
+    linux_fx_version = "DOTNETCORE|8.0"
+  }
 }
 
-resource "azurerm_app_service_slot" "example" {
+resource "azurerm_linux_web_app_slot" "example" {
   name                = "${var.company}${var.environment}asstaging"
-  app_service_name    = azurerm_app_service.example.name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_app_service_plan.example.id
+  app_service_id    = azurerm_app_service.example.id
+  site_config {
+    linux_fx_version = "DOTNETCORE|8.0"
+  }
 }
